b8b6b07dcabe0524c1084aeac629fa67d25b9031
see91 <isee_wang@outlook.com>
2019-11-24 00:20:14 +0800

    ⚙️vuex ts setting

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,18 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { StoreOptions } from "vuex";
+import { RootState } from './types'
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
+const store: StoreOptions<RootState> = {
+  state: {
+    address: '',
+    gas: 4000000,
+    gasPrice: 90000000000,
+  },
   mutations: {},
   actions: {},
   modules: {}
-});
+}
+
+export default new Vuex.Store<RootState>(store)
---
+++ src/store/types.ts
@@ -0,0 +1,5 @@
+export interface RootState {
+  gas: number,
+  gasPrice: number,
+  address: string
+}
