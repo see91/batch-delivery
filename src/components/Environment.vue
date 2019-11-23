c09c6c81f2b44b72079e2772f8985fb730653ba9
see91 <isee_wang@outlook.com>
2019-11-24 00:19:55 +0800

    ⎚ clear + construct

---
+++ src/components/Environment.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="env">
+    <a-button @click="changeEnv">{{ env_desc[env] }}</a-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+enum env {
+  pro,
+  test
+}
+
+@Component
+export default class Environment extends Vue {
+  private env_desc = {
+    true: "正式",
+    false: "测试"
+  };
+  private env: Boolean = true;
+  changeEnv() {
+    this.env = !this.env;
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+#env
+  border 2px solid red
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="home">
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <Environment />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Environment from "@/components/Environment.vue";
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+@Component({
+  components: { Environment }
+})
+export default class Home extends Vue {
+  msg: string = "This is Home!";
+}
 </script>
+
+<style lang="stylus">
+#home
+  width 100%
+  height 100vh
+  padding-top 60px
+</style>
